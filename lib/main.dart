e5b1316b9f0a2d32577e5d09ef7b939d0edfafe9
NGHTMR3 <git@nghtmre.dev>
2022-04-08 22:58:46 -0600

    corrected the outdated configuration for the Android v2 embed and API version completed the magic 8 ball mini project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,60 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade800,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballSelection = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballSelection = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                shakeBall();
+              },
+              child: Image.asset('images/ball$ballSelection.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
